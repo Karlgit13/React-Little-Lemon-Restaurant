66c4066e413cdc3ecea63ed2ea92b8aed532b46c
Karlgit13 <karl.vareskog@gmail.com>
2024-01-06 15:19:23 +0100

    progress

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./main.css";
 import FoodImg from "../assets/restauranfood.jpg";
 import ArticleCards from "./ArticleCards";
+import marioAndAdrian1 from "../assets/Mario and Adrian A.jpg";
+import marioAndAdrian2 from "../assets/Mario and Adrian b.jpg";
+import restaurantChef from "../assets/restaurant chef B.jpg";
+import restaurantImg from "../assets/restaurant.jpg";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
@@ -14,9 +19,17 @@ const Main = () => {
             We are a family owned Mediterranean restaurant, focused on
             traditional recipes served with a modern twist.
           </p>
-          <img className="foodImg" src={FoodImg} alt="foodImg" />
+          <img className="mobileImg" src={FoodImg} alt="mobileImg" />
         </div>
-        <button className="mainButton">Reserve a table</button>
+        <div className="desktopImgContainer">
+          <img className="desktopImg1" src={marioAndAdrian1} alt="desktopImg" />
+          <img className="desktopImg2" src={marioAndAdrian2} alt="desktopImg" />
+          <img className="desktopImg3" src={restaurantChef} alt="desktopImg" />
+          <img className="desktopImg4" src={restaurantImg} alt="desktopImg" />
+        </div>
+        <Link className="LinkClassName" to="/Order">
+          <button className="mainButton">Reserve a table</button>
+        </Link>
       </div>
       <div className="menuContainer">
         <h1 className="mainOrderTitle">ORDER FOR DELIVERY!</h1>
